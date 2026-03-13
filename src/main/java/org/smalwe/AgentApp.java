bf1061b63e83c6a32133525ede2d772d393b2a98
deadzg <sunny.malwe6@gmail.com>
2026-03-13 09:13:41 +0000

    Adding tool call

--- src/main/java/org/smalwe/AgentApp.java
+++ src/main/java/org/smalwe/AgentApp.java
@@ -12,9 +12,7 @@ import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.request.ChatRequest;
 import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
-import dev.langchain4j.model.output.Response;
 import dev.langchain4j.service.tool.DefaultToolExecutor;
-import dev.langchain4j.service.tool.ToolExecutionResult;
 import dev.langchain4j.service.tool.ToolExecutor;
 
 import java.util.Arrays;
@@ -33,58 +31,7 @@ public class AgentApp {
                 .modelName(GEMINI_MODEL_NAME)
                 .build();
 
-        List<ToolSpecification> toolSpecifications = ToolSpecifications.toolSpecificationsFrom(MathTool.class);
-
-
-        String systemInputText = """
-                                    You are a Mathematics teach in a school. 
-                                    You always amend response with a funny quote about maths.
-                                """;
-        SystemMessage systemMessage = SystemMessage.from(systemInputText);
-
-        String userInputText = "I have 500 oranges and 200 bananas. How many total fruits I have?";
-        UserMessage userMessage = UserMessage.from(userInputText);
-
-        List<ChatMessage> messages = Arrays.asList(systemMessage, userMessage);
-
-        ChatRequest chatRequest = ChatRequest.builder()
-                                    .messages(messages)
-                                    .toolSpecifications(toolSpecifications)
-                                    .build();
-
-
-        ChatResponse chatResponse = model.chat(chatRequest);
-
-        if(chatResponse.aiMessage().hasToolExecutionRequests()) {
-            List<ToolExecutionRequest> toolExecutionRequests = chatResponse.aiMessage().toolExecutionRequests();
-            for(ToolExecutionRequest toolExecutionRequest: toolExecutionRequests) {
-                MathTool mathTool = new MathTool();
-                ToolExecutor toolExecutor = new DefaultToolExecutor(mathTool, mathTool.getClass().getDeclaredMethod(
-                        toolExecutionRequest.name(), int.class, int.class));
-                Long result = Long.valueOf(toolExecutor.execute(toolExecutionRequest, Long.class));
-                System.out.println("Result:" + result);
-
-                ToolExecutionResultMessage toolExecutionResultMessage = ToolExecutionResultMessage.from(toolExecutionRequest, Long.toString(result));
-                ChatRequest chatRequest2 = ChatRequest.builder()
-                        .messages(List.of(userMessage, chatResponse.aiMessage(), toolExecutionResultMessage))
-                        .toolSpecifications(toolSpecifications)
-                        .build();
-                ChatResponse chatResponse2 = model.chat(chatRequest2);
-
-                System.out.println(String.format("Final AiMessage:%s", chatResponse2.aiMessage()));
-            }
-        }
-
-        System.out.println(String.format("InputAiMessage: %s \nAiMessage:%s", userInputText, chatResponse.aiMessage()));
-        /* Below is a sample output:
-        InputAiMessage: Add two number: 5, 6
-        AiMessage:AiMessage { text = "5 + 6 = 11", thinking = null, toolExecutionRequests = [], attributes = {} }
-         */
-        // Accessing metadata via the .metadata() sub-object
-        System.out.println("Model used: " + chatResponse.metadata().modelName()); //Model used: gemini-2.5-flash
-        System.out.println("Input tokens: " + chatResponse.metadata().tokenUsage().inputTokenCount()); //Input tokens: 36
-        System.out.println("Output tokens: " + chatResponse.metadata().tokenUsage().outputTokenCount()); //Output tokens: 205
-        System.out.println("Finish Reason: " + chatResponse.metadata().finishReason()); //Finish Reason: STOP
+        llmCallUsingToolCall(model);
     }
 
     /**
@@ -114,12 +61,79 @@ public class AgentApp {
 
         ChatResponse chatResponse = chatModel.chat(messages);
 
-        System.out.println(String.format("InputAiMessage: %s \nAiMessage:%s", userInputText, chatResponse.aiMessage()));
-        /* Below is a sample output:
-        InputAiMessage: Add two number: 5, 6
-        AiMessage:AiMessage { text = "5 + 6 = 11", thinking = null, toolExecutionRequests = [], attributes = {} }
+        printChatResponse(chatResponse);
+    }
+
+    public static void llmCallUsingToolCall(ChatModel chatModel) throws NoSuchMethodException {
+        String systemInputText = """
+                                    You are a Mathematics teach in a school. 
+                                    You always amend response with a funny quote about maths.
+                                """;
+        SystemMessage systemMessage = SystemMessage.from(systemInputText);
+
+        String userInputText = "I have 500 oranges and 200 bananas. How many total fruits I have?";
+        UserMessage userMessage = UserMessage.from(userInputText);
+
+        //Specify set of tools you would like LLM to pick from
+        List<ToolSpecification> toolSpecifications = ToolSpecifications.toolSpecificationsFrom(MathTool.class);
+
+        // Prepare list of messages: system and user you need to pass to LLM
+        List<ChatMessage> messages = Arrays.asList(systemMessage, userMessage);
+
+        // Create a chat request with messages and tool specification
+        ChatRequest chatRequest = ChatRequest.builder()
+                .messages(messages)
+                .toolSpecifications(toolSpecifications)
+                .build();
+
+
+        // Call LLM with the chatRequest message
+        ChatResponse chatResponse = chatModel.chat(chatRequest);
+        printChatResponse(chatResponse);
+        /*
+           Check if the response has the tool execution requests
+
+           Sample AIMessage Looks like below:
+           AiMessage { text = null, thinking = null, toolExecutionRequests = [ToolExecutionRequest { id = null, name = "addNumbers", arguments = "{"arg1":200,"arg0":500}" }], attributes = {} }
          */
-        // Accessing metadata via the .metadata() sub-object
+        if(chatResponse.aiMessage().hasToolExecutionRequests()) {
+
+            // Fetch all the tool executions LLM has determined based on the chatRequest
+            List<ToolExecutionRequest> toolExecutionRequests = chatResponse.aiMessage().toolExecutionRequests();
+
+            //For each tool call the appropriate tool
+            for(ToolExecutionRequest toolExecutionRequest: toolExecutionRequests) {
+                // Create an instance of math tool
+                MathTool mathTool = new MathTool();
+
+                // Create a default tool executor based on the name and parameters of the tool method
+                ToolExecutor toolExecutor = new DefaultToolExecutor(mathTool, mathTool.getClass().getDeclaredMethod(
+                        toolExecutionRequest.name(), int.class, int.class));
+
+                // Store the result of tool execution
+                Long result = Long.valueOf(toolExecutor.execute(toolExecutionRequest, Long.class));
+                System.out.println("Result:" + result);
+
+                // Create the result message so that it could be sent back to LLM
+                ToolExecutionResultMessage toolExecutionResultMessage = ToolExecutionResultMessage.from(toolExecutionRequest, Long.toString(result));
+
+                // Create another chatRequest with the tool result. Make sure you send all the tools and messages again in this request along with tool result
+                ChatRequest chatRequest2 = ChatRequest.builder()
+                        .messages(List.of(userMessage, chatResponse.aiMessage(), toolExecutionResultMessage))
+                        .toolSpecifications(toolSpecifications)
+                        .build();
+
+                // Call the LLM
+                ChatResponse chatResponse2 = chatModel.chat(chatRequest2);
+
+                // Print the LLM response
+                printChatResponse(chatResponse2);
+            }
+        }
+    }
+
+    private static void printChatResponse(ChatResponse chatResponse) {
+        System.out.println(String.format("AiMessage:%s", chatResponse.aiMessage()));
         System.out.println("Model used: " + chatResponse.metadata().modelName()); //Model used: gemini-2.5-flash
         System.out.println("Input tokens: " + chatResponse.metadata().tokenUsage().inputTokenCount()); //Input tokens: 36
         System.out.println("Output tokens: " + chatResponse.metadata().tokenUsage().outputTokenCount()); //Output tokens: 205
